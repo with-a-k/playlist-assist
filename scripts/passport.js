5e17b1dba0148af38d61b4a0b9ea8b25498c15cc
Erik Butcher <with_a_k@mac.com>
2020-12-09 21:10:39 -0700

    Fix asynchronous db access

--- scripts/passport.js
+++ scripts/passport.js
@@ -19,7 +19,7 @@ passport.use(new SpotifyStrategy({
   callbackURL: "https://immense-coast-83178.herokuapp.com/auth/spotify/callback"
 },
 function(accessToken, refreshToken, profile, done) {
-  userIndex = await database.findOrCreateUser(profile.id);
+  userIndex = database.findOrCreateUser(profile.id);
   database.updateUserTokens(userIndex, accessToken, refreshToken);
   return done(null, profile);
 }
