e6689a3b88b0458faf2e4b8c667dd7c51cf8410f
Erik Butcher <with_a_k@mac.com>
2020-12-14 20:19:02 -0700

    Tear out Passport and roll my own

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const passport = require('passport');
+const axios = require('axios');
+const querystring = require('querystring');
 
 router.get('/', (req, res, next) => res.redirect('/'));
 
 router.get('/error', (req, res, next) => res.send('Unknown Error'));
 
-router.get('/spotify', passport.authenticate('spotify'));
-
-router.get('/spotify/callback', passport.authenticate('spotify',
-  {
-    scope: ['playlist-read-private', 'playlist-read-collaborative',
-      'playlist-modify-public', 'playlist-modify-private'],
-    failureRedirect: '/auth/error'
-  }),
-function(req, res) {
-  res.redirect('/tokens');
+router.get('/spotify', function(req, res, next) {
+  axios.get('https://accounts.spotify.com/authorize/',
+    querystring.stringify({
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      response_type: 'code',
+      redirect_uri: 'https://immense-coast-83178.herokuapp.com/auth/spotify/callback',
+      state: 'arbitrary',
+      scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private',
+    }));
 });
+
+router.get('/spotify/callback', (req, res, next) => res.send('Logged in?'););
 
 module.exports = router;
