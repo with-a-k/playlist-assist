2d7c6c53218dc37953e1b29a1f002aa9abdf7328
Erik Butcher <with_a_k@mac.com>
2020-12-14 20:27:18 -0700

    Switch querystring to urlsearchparams

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,21 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const querystring = require('querystring');
+const url = require('url');
 
 router.get('/', (req, res, next) => res.redirect('/'));
 
 router.get('/error', (req, res, next) => res.send('Unknown Error'));
 
 router.get('/spotify', function(req, res, next) {
-  axios.get('https://accounts.spotify.com/authorize/',
-    querystring.stringify({
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      response_type: 'code',
-      redirect_uri: 'https://immense-coast-83178.herokuapp.com/auth/spotify/callback',
-      state: 'arbitrary',
-      scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private',
-    }));
+  params = new url.URLSearchParams({
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: 'https://immense-coast-83178.herokuapp.com/auth/spotify/callback',
+    state: 'arbitrary',
+    scope: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private',
+  })
+  axios.get('https://accounts.spotify.com/authorize/', params.toString());
 });
 
 router.get('/spotify/callback', (req, res, next) => res.send('Logged in?'));
