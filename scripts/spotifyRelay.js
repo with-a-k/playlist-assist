f75902643c859da70aaf79b04d529b8b4e02d163
Erik Butcher <with_a_k@mac.com>
2020-12-15 12:11:58 -0700

    Fix promise chaining in get profile

--- scripts/spotifyRelay.js
+++ scripts/spotifyRelay.js
@@ -38,7 +38,7 @@ function refreshAccessToken() {
 }
 
 function getProfile(req, res, next) {
-  spotifyApi.getMe(),then(function(data) {
+  spotifyApi.getMe().then(function(data) {
     console.log('This is the user logging in:');
     console.log(data.body);
     req.session.user = data.body;
