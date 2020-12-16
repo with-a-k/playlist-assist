64cf0213b77526dd1607ec9ddc82fc8256bfcf89
Erik Butcher <with_a_k@mac.com>
2020-12-15 22:35:43 -0700

    Fix data gathering 4

--- public/javascripts/remoteCalls.js
+++ public/javascripts/remoteCalls.js
@@ -44,10 +44,10 @@ function loadTracksFromPlaylist(playlistId) {
 
 function getAnalysis() {
   console.log(Object.values($('.track-added')));
-  let tracksData = Object.values($('.track-added')).map(function(track) {
-    if (typeof track !== 'HTMLElement') {
-      return false;
-    }
+  let tracksData = Object.values($('.track-added')).filter(function(track) {
+    console.log(track);
+    console.log(typeof track);
+  }).map(function(track) {
     trackName = track.children.item(0).innerHTML;
     artistList = track.children.item(1).innerHTML.slice(9);
     albumName = track.children.item(2).innerHTML.slice(7);
